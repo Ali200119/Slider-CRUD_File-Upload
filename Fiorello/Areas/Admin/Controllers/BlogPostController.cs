7cce2445e9b882091b24e939249cc1fdecd05b3a
Ali200119 <aliit@code.edu.az>
2023-04-24 00:17:43 +0400

    Slider CRUD

---
+++ Fiorello/Areas/Admin/Controllers/BlogPostController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello.Areas.Admin.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ Fiorello/Helpers/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Fiorello.Helpers
+{
+	public static class FileHelper
+	{
+		public static bool CheckFileType(this IFormFile file, string pattern)
+		{
+			return file.ContentType.Contains(pattern);
+		}
+
+		public static void DeleteFileFromPath(string path)
+		{
+			if (File.Exists(path)) File.Delete(path);
+		}
+	}
+}
